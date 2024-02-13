78afad6ea833e615d54ddcd963f6e085c14b7b9b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-02-13 10:04:42 +0000

    Added installation script for dependencies

---
+++ web_flask/dependecies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# A bash scripts that installs necessary dependencies
+
+# install google api client
+pip install google-api-python-client;
+
+# install python flask framework
+pip install Flask;
+
+# install wtf flask extension
+pip install Flask-WTF;
+# install Gunicorn
+pip install gunicorn;
